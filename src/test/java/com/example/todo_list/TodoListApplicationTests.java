90a566e6a54947e1e356eeb34730396864edf7c9
yuhyun1 <yh7119@gmail.com>
2023-05-31 03:35:49 +0900

    배포 세팅

--- src/test/java/com/example/todo_list/TodoListApplicationTests.java
+++ src/test/java/com/example/todo_list/TodoListApplicationTests.java
@@ -3,7 +3,7 @@ package com.example.todo_list;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class TodoListApplicationTests {
 
     @Test
